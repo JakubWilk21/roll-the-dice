0c7493db61d4592fe4d8f5f7bae1827bc1abd5f8
Jakub <Wilk>
2022-11-07 09:41:28 -0600

    Initial Commit

---
+++ roll the dice/roll_the_diceApp.swift
@@ -0,0 +1,17 @@
+//
+//  roll_the_diceApp.swift
+//  roll the dice
+//
+//  Created by Jakub Wilk on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct roll_the_diceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
